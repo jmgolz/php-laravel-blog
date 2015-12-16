2b2bd244a4039e193cfe3f89f3ce2aee6c32e422
James Golz <jmgolz@gmail.com>
2015-12-16 18:46:31 +0000

    super basic api/json tutorial lesson in Laravel.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -79,3 +79,38 @@ Route::get('/delete', function(){
        echo $categories_list_item->id . ' ' . $categories_list_item->name . '<br />';
    }
 });
+
+//API building part of Laravel tutorial
+Route::get('/api/v1/products/{id?}', array(
+   'middleware' => 'auth.basic',
+   function($id = null){
+       if($id == null){
+           $products = App\Product::all(array( 'id', 'name', 'price' ));
+       } else {
+           $products = App\Product::find($id, array( 'id', 'name', 'price' ));
+       }
+       
+       return Response::json(array(
+            'error' => false,
+            'products' => $products,
+            'status_code' => 200
+        ));
+   }
+));
+
+Route::get('/api/v1/categories/{id?}', array(
+   'middleware' => 'auth.basic',
+   function($id = null){
+       if($id == null){
+           $categories = App\Category::all(array( 'id', 'name' ));
+       } else {
+           $categories = App\Category::find($id, array( 'id', 'name' ));
+       }
+       
+       return Response::json(array(
+           'error' => false,
+           'user'  => $categories,
+           'status_code' => 200
+        ));
+   }
+));
