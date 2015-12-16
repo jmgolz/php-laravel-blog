863e19e42b9a144e9c9ef207b4a66d78c7ea86e2
James Golz <jmgolz@gmail.com>
2015-12-16 21:19:19 +0000

    update front controller code functions to return blog posts

--- app/Http/Controllers/Front.php
+++ app/Http/Controllers/Front.php
@@ -53,11 +53,30 @@ class Front extends Controller
     }
 
     public function blog() {
-        return view('blog', array('title' => 'Welcome','description' => '','page' => 'blog', 'brands' => $this->brands, 'categories' => $this->categories, 'products' => $this->products));
+        $posts = Post::where('id', '>', 0)->paginate(3);
+        $posts->setPath('blog');
+        
+        $data['posts'] = $posts;
+        return view('blog', array('data' => $data, 'title' => 'Latest Blog Posts', 'description' => '', 'page' => 'blog', 'brands' => $this->brands, 'categories' => $this->categories, 'products' => $this->products));
     }
 
     public function blog_post($id) {
-        return view('blog_post', array('title' => 'Welcome','description' => '','page' => 'blog', 'brands' => $this->brands, 'categories' => $this->categories, 'products' => $this->products));
+        //return view('blog_post', array('title' => 'Welcome','description' => '','page' => 'blog', 'brands' => $this->brands, 'categories' => $this->categories, 'products' => $this->products));
+        $post = Post::whereUrl($url)->first();
+    
+        $tags = $post->tags;
+        $prev_url = Post::prevBlogPostUrl($post->id);
+        $next_url = Post::nextBlogPostUrl($post->id);
+        
+        $title = $post->title;
+        $description = $post->description;
+        $page = 'blog';
+        $brands = $this->brands;
+        $categories = $this->categories;
+        $products = $this->products;
+    
+        $data = compact('prev_url', 'next_url', 'tags', 'post', 'title', 'description', 'page', 'brands', 'categories', 'products');
+        return view('blog_post', $data);
     }
 
     public function contact_us() {
